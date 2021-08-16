3fa4ab4ab51bf3c263433d0bc0aabbb163bc5d45
masaka27 <15242577+masaka27@users.noreply.github.com>
2021-08-17 07:10:44 +0900

    TOD0１回目

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "row";
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // 完了ボタン生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+  //  削除ボタン生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // alert("削除");
+    // 押された削除ボタンの親タグ(li)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    // console.log(deleteTarget);
+    document.getElementById("list-a").removeChild(deleteTarget);
+  });
+
+  // ulタグの子要素に各要素を追加
+  const ul_a = document.getElementById("list-a");
+  ul_a.appendChild(li);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+};
+
+document.getElementById("add-btn").addEventListener("click", () => {
+  onClickAdd();
+});
